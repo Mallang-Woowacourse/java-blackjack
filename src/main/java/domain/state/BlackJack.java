57f9ad8b4d146c28402ab39d9916323463b68382
shin-mallang <huipulci1@naver.com>
2023-03-10 12:47:02 +0900

    feat: BlackJack 상태 구현

--- src/main/java/domain/state/BlackJack.java
+++ src/main/java/domain/state/BlackJack.java
@@ -1,10 +1,17 @@
 package domain.state;
 
 import domain.card.CardArea;
+import domain.player.BettingMoney;
+import domain.player.Revenue;
 
 public class BlackJack extends BlackJackGameState {
 
     protected BlackJack(final CardArea cardArea) {
         super(cardArea);
+    }
+
+    @Override
+    public Revenue revenue(final BettingMoney bettingMoney) {
+        return Revenue.blackJackWin(bettingMoney);
     }
 }
